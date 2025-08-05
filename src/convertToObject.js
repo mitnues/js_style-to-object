43087074d19cb95cb72b9d5d629da84c738577cc
milton <mitnunes.pro.com>
2025-08-05 17:22:15 -0300

    first add

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,25 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const resultStrs = sourceString.split(' ');
+  const finalResult = [];
+  const finalObjct = {
+    key,
+    value,
+  };
+
+  for (const result of resultStrs) {
+      finalResult.push(result);
+  }
+
+  for (let i = 0; i < finalResult.length; i++) {
+    finalObjct.key = finalResult[i];
+    finalObjct.value = finalResult[i + 1];
+  }
+  console.log(finalObjct);
+  return finalObjct;
+
 }
 
 module.exports = convertToObject;
+
